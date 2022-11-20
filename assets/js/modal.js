71b98b66e05f38ef277da31be93ef70145232613
Jack112-create <jackconroy777@gmail.com>
2022-11-20 21:11:54 +0000

    Update modal logic and style contributors modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,25 +1,13 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+let modalButtons = document.getElementsByClassName('modal-btn');
+let modals = document.getElementsByClassName('modal');
+let modalCloseButtons = document.getElementsByClassName('close');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+for (let i = 0; i < modalButtons.length; i++) {
+    modalButtons[i].addEventListener('click', function() {
+        modals[i].style.display = 'block';
+    });
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    modalCloseButtons[i].addEventListener('click', function() {
+        modals[i].style.display = 'none';
+    });
 }
