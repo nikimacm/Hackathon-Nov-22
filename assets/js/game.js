e6646269e01c17fa33bc7df57fb672bf3cb39fd2
TuckerFaulk <103144577+TuckerFaulk@users.noreply.github.com>
2022-11-20 17:29:59 +0000

    Linked up the js event listeners to the click boxes (#26)

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,87 +12,112 @@ function countdown() {
     tick();
   }
 
-// countdown();
+countdown();
 
 /**
- * Notes from TFaulk:
- * This is the bones of the game logic
- * Once I have the final background image,
- * I will be able to properly wire it up
- * Apologies that it is messy atm, but I will tidy this up
- * Thanks
+ * Main Game Play Function
+ * A random number is selected. 
+ * This corresponds to an instrument in the switch.
+ * This number is then removed from the randomCharacter array (so it can not be reselected)
+ * The gamePlay function is re-run for another character to be guessed.
  */
 
-let randomCharacter = [1, 2, 3, 4, 5];
-let num1 = 5; // Update the name of this var
+// Global Variables (To be moved to the top of the page)
+let randomCharacter = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numOfCharacters = 9;
+const bongo = document.getElementById("bongo");
+const cello = document.getElementById("cello");
+const childSing = document.getElementById("child-sing");
+const drum = document.getElementById("drum");
+const flute = document.getElementById("flute");
+const guitar = document.getElementById("guitar");
+const piano = document.getElementById("piano");
+const trombone = document.getElementById("trombone");
+const violin = document.getElementById("violin");
 
 function gamePlay() {
 
-  console.log("Start Game Play");
-
-  background.removeEventListener('click', gamePlay); // Make sure you remove the even listeners 
+  // Removes all of the event listeners from the previous selection
+  bongo.removeEventListener('click', gamePlay);
+  cello.removeEventListener('click', gamePlay);
+  childSing.removeEventListener('click', gamePlay);
+  drum.removeEventListener('click', gamePlay);
+  flute.removeEventListener('click', gamePlay);
+  guitar.removeEventListener('click', gamePlay);
+  piano.removeEventListener('click', gamePlay);
+  trombone.removeEventListener('click', gamePlay);
+  violin.removeEventListener('click', gamePlay);
 
   let counter = document.getElementById("timer").innerHTML;
-  console.log(counter);
-
-  console.log("randomCharacter.length:", randomCharacter.length);
   
   if (randomCharacter.length !== 0) {
     if (counter !== "0:00") {
-      
-      console.log("Start if loop");
 
-      let num = Math.floor(Math.random() * num1);
-      console.log("num", num);
-
-      let chosenCharacter = randomCharacter[num];
-      console.log("chosenCharacter", chosenCharacter);
-
-      const background = document.getElementById("background");
+      let randomNum = Math.floor(Math.random() * numOfCharacters);
+      let chosenCharacter = randomCharacter[randomNum];
 
       switch (chosenCharacter) {
           case 1:
-              console.log("CASE 1");
-              randomCharacter.splice(num, 1);
-              num1--;
-              console.log(randomCharacter);
-              background.addEventListener('click', gamePlay);
+              console.log("CASE 1: Bongo");
+              randomCharacter.splice(randomNum, 1);
+              numOfCharacters--;
+              bongo.addEventListener('click', gamePlay);
               break;
           case 2:
-              console.log("CASE 2");
-              randomCharacter.splice(num, 1);
-              num1--;
-              console.log(randomCharacter);
-              background.addEventListener('click', gamePlay);
+              console.log("CASE 2: Cello");
+              randomCharacter.splice(randomNum, 1);
+              numOfCharacters--;
+              cello.addEventListener('click', gamePlay);
               break;
           case 3:
-              console.log("CASE 3");
-              randomCharacter.splice(num, 1);
-              num1--;
-              console.log(randomCharacter);
-              background.addEventListener('click', gamePlay);
+              console.log("CASE 3: Sing");
+              randomCharacter.splice(randomNum, 1);
+              numOfCharacters--;
+              childSing.addEventListener('click', gamePlay);
               break;
           case 4:
-              console.log("CASE 4");
-              randomCharacter.splice(num, 1);
-              num1--;
-              console.log(randomCharacter);
-              background.addEventListener('click', gamePlay);
+              console.log("CASE 4: Drum");
+              randomCharacter.splice(randomNum, 1);
+              numOfCharacters--;
+              drum.addEventListener('click', gamePlay);
               break;
           case 5:
-              console.log("CASE 5");
-              randomCharacter.splice(num, 1);
-              num1--;
-              console.log(randomCharacter);
-              background.addEventListener('click', gamePlay);
+              console.log("CASE 5: Flute");
+              randomCharacter.splice(randomNum, 1);
+              numOfCharacters--;
+              flute.addEventListener('click', gamePlay);
+              break;
+          case 6:
+              console.log("CASE 6: Guitar");
+              randomCharacter.splice(randomNum, 1);
+              numOfCharacters--;
+              guitar.addEventListener('click', gamePlay);
+              break;
+          case 7:
+              console.log("CASE 7: Piano");
+              randomCharacter.splice(randomNum, 1);
+              numOfCharacters--;
+              piano.addEventListener('click', gamePlay);
+              break;
+          case 8:
+              console.log("CASE 8: Trombone");
+              randomCharacter.splice(randomNum, 1);
+              numOfCharacters--;
+              trombone.addEventListener('click', gamePlay);
+              break;
+          case 9:
+              console.log("CASE 9: Violin");
+              randomCharacter.splice(randomNum, 1);
+              numOfCharacters--;
+              violin.addEventListener('click', gamePlay);
               break;
       }
     } else {
       console.log("Game Won");
     }
   } else {
-    console.log("Game Won");
+    console.log("Game End");
   }
 }
 
-// gamePlay();
+gamePlay();
